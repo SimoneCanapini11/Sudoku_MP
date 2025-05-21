311b3ef0b4d9b5fb9fd73b2ddb854982121fc801
SimoneCanapini11 <simonecanapini11@gmail.com>
2025-05-21 16:43:09 +0200

    commit 12

--- Main.kt
+++ Main.kt
@@ -1,10 +1,3 @@
 class Main {
     //prova cana mixtape pt.4
-    fun main() {
-        // Definiamo il saluto
-        val saluto = "Ciao, mondomarcio!"
-
-        // Stampiamo il saluto
-        println(saluto)
-    }
 }
