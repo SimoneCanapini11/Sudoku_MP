75c501eb62fe15b5d3af82753d1522e89934493e
LorenzoGrasselli30 <lorenzo.grasselli30@gmail.com>
2025-05-21 16:32:46 +0200

    Prova cana mixtape1

--- Main.kt
+++ Main.kt
@@ -1,3 +1,10 @@
 class Main {
     //prova cana mixtape pt.4
+    fun main() {
+        // Definiamo il saluto
+        val saluto = "Ciao, mondomarcio!"
+
+        // Stampiamo il saluto
+        println(saluto)
+    }
 }
